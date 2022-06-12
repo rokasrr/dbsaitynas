0163357ded8c0f5a56ad8a2afbc7da103e9d9205
rokasrr <hakatt@gmail.com>
2022-06-12 22:42:25 +0300

    add user rest controller

--- src/main/java/lt/rokas/db/kirneilio/controller/rest/UserController.java
+++ src/main/java/lt/rokas/db/kirneilio/controller/rest/UserController.java
@@ -2,68 +2,49 @@ package lt.rokas.db.kirneilio.controller.rest;
 
 import lt.rokas.db.kirneilio.entity.User;
 import lt.rokas.db.kirneilio.service.UserService;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class UserController {
 
+    @Autowired
     private UserService userService;
 
     public UserController(UserService userService) {
         super();
+
         this.userService = userService;
     }
 
     @GetMapping("/users")
-    public String listUsers(Model model) {
-        model.addAttribute("users", userService.getAllUsers());
-        return "users";
+    public List<User> listUsers() {
+        return userService.getAllUsers();
     }
 
-    @GetMapping("/users/new")
-    public String createUserForm(Model model) {
-
-        User user = new User();
-        model.addAttribute("user", user);
-        return "create_user";
-
-    }
-
-    @PostMapping("/users")
-    public String saveUser(@ModelAttribute("user") User user) {
+    @PostMapping("/users/new")
+    public ResponseEntity<HttpStatus> createUser(@RequestBody User user) {
         userService.saveUser(user);
-        return "redirect:/users";
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 
-    @GetMapping("/users/edit/{id}")
-    public String editUserForm(@PathVariable Long id, Model model) {
-        model.addAttribute("user", userService.getUserById(id));
-        return "edit_user";
+    @PostMapping("/users/edit/{id}")
+    public ResponseEntity<HttpStatus> updateUser(@PathVariable Long id, @RequestBody User user) {
+        userService.updateUser(user);
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 
-    @PostMapping("/users/{id}")
-    public String updateUser(@PathVariable Long id,
-                             @ModelAttribute("user") User user,
-                             Model model) {
-
-        User existingUser = userService.getUserById(id);
-        existingUser.setId(id);
-        existingUser.setFirstName(user.getFirstName());
-        existingUser.setEmail(user.getEmail());
-
-        userService.updateUser(existingUser);
-        return "redirect:/users";
-    }
-
-
-    @GetMapping("/users/{id}")
-    public String deleteUser(@PathVariable Long id) {
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable Long id) {
         userService.deleteUserById(id);
-        return "redirect:/users";
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
     }
 }
